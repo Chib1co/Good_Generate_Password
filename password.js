5fd31843219127d1b69365525d08782dfde2cb0c
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2020-11-30 11:27:49 +1100

    first commit

---
+++ password.js
@@ -0,0 +1,36 @@
+//user confirm password criteria
+//confirm lower case
+let lowerCaseChar = confirm("Use Lowercase charactors?");
+//confirm upper case
+let upperCaseChar = confirm("Use Uppercase charactors?");
+//confirm numbers
+let numberChar = confirm("Use Numbers?");
+//confirm symbols
+let symbolChar = confirm("Use symbols?");
+
+//when user click generate botton
+addEventListener
+
+//generate password
+function generate(){
+
+let complexity = document.getElementById("slider").value;
+
+let vlues = "abcdefghijklmnopqstvxyzABCDEFGHIKLMNOPQRSTVXYZ123456789#$%&-=_";
+
+let dataset = "";
+
+//clock the bottum to generate the password
+
+//Condition to making charactors
+
+
+//computer pick ramdom charactor up to 10
+for(var i=0; i <= complexity.length; i++){
+var passWord = dataset + value.charAt(Math.floor(Math.ramdom() * Math.floow(values.length -1)));
+
+}
+
+document.getElementById("display").value = passWord;
+
+}
